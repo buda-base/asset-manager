929754201ca9fc73738831914eb06a476c47c418
Jim Katz <jimk@tbrc.org>
2019-05-09 10:49:24 -0400

    release snapshot 2
    Downgrade release to 0.8-SNAPSHOT-2

---
+++ audittool/toInstall.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cp audit-test-lib/target/output/audit-test-lib-1.0-SNAPSHOT-jar-with-dependencies.jar install
+cp audit-test-shell/target/output/audit-test-shell-1.0-SNAPSHOT.jar install
