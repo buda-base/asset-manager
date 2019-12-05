7aaa402d1893b4fc4b801753379ed576be36ab46
Jim Katz <jimk@tbrc.org>
2019-12-05 18:37:52 -0500

    WIP reference policy

--- audittool/audit-test-shell/src/main/scripts/audittool.ps1
+++ audittool/audit-test-shell/src/main/scripts/audittool.ps1
@@ -36,5 +36,17 @@ $LOG_PROPS = Join-Path -Path $CONFIG_ATHOME -ChildPath log4j2.properties
 # This is the system itself. Do not change it
 $shellJar = $CONFIG_SHELL_JAR_FILE
 
+# Unless you're goin to hack it
+# $shellJar = "C:\Users\jsk\dev\asset-manager\audittool\audit-test-shell\target\output\audit-test-shell-0.9-SNAPSHOT-1.jar"
+# no -Djava.security.manager "-Djava.security.policy=.\test.policy -DtestJar=$CONFIG_TEST_LIB_JAR_FILE" "-Dlog4j.configurationFile=$LOG_PROPS"
+# java  "-DtestJar=$CONFIG_TEST_LIB_JAR_FILE -Dlog4j.configurationFile=$LOG_PROPS" -jar $shellJar $args
+java "-Djava.security.manager -Djava.security.policy=${CONFIG_ATHOME}.\audit-tool.policy "  "-DtestJar=$CONFIG_TEST_LIB_JAR_FILE" "-Dlog4j.configurationFile=$LOG_PROPS" -jar $shellJar $args
+# Try without jar, and without interface jar in current dir
+# nope - PS C:\tmp> C:\users\jsk\Desktop\adt9\audittool.ps1trello
+# Error: Could not find or load main class io.bdrc.am.audit.shell.shell
+# java -classpath C:\Users\jsk\dev\asset-manager\audittool\audit-test-shell\target\output\audit-test-interface-0.9-SNAPSHOT-1.jar "-DtestJar=$CONFIG_TEST_LIB_JAR_FILE" "-Dlog4j.configurationFile=$LOG_PROPS" io.bdrc.am.audit.shell.shell $args
+# -classpath C:\Users\jsk\dev\asset-manager\audittool\audit-test-shell\target\output\audit-test-interface-0.9-SNAPSHOT-1.jar
 
-java "-DtestJar=$CONFIG_TEST_LIB_JAR_FILE" "-Dlog4j.configurationFile=$LOG_PROPS" -jar $shellJar $args
+#pushd  C:\Users\jsk\dev\asset-manager\audittool\audit-test-shell\target\output\
+#java  "-DtestJar=$CONFIG_TEST_LIB_JAR_FILE" "-Dlog4j.configurationFile=$LOG_PROPS" -jar $shellJar $args
+#popd
