064b4fc5c10e77066785bff8734dc82ca13ab251
Jim Katz <jimk@tbrc.org>
2020-06-08 13:54:23 -0400

    Remove some artifacts

--- src/main/java/io/bdrc/assetmanager/repositories/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package io.bdrc.assetmanager.repositories;
-
-import io.bdrc.assetmanager.entities.Employee;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
-    }
