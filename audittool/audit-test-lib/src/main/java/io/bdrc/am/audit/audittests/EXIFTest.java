6b7e41a82ca0465595ee6f281bbe5a952a69cbc3
Jim Katz <jimk@tbrc.org>
2021-02-08 18:30:48 -0500

    Fixes #102
    parseInt gags on spaces. Trim input from properties.file

--- audittool/audit-test-lib/src/main/java/io/bdrc/am/audit/audittests/EXIFTest.java
+++ audittool/audit-test-lib/src/main/java/io/bdrc/am/audit/audittests/EXIFTest.java
@@ -3,7 +3,6 @@ package io.bdrc.am.audit.audittests;
 import io.bdrc.am.audit.iaudit.LibOutcome;
 import io.bdrc.am.audit.iaudit.Outcome;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
