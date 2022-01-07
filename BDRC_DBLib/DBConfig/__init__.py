9201499c3112bb4c7a60d871f2fbb57328c4f155
Jim Katz <jimk@tbrc.org>
2022-01-07 10:56:16 -0500

    Clean up before merging main

--- BDRC_DBLib/DBConfig/__init__.py
+++
@@ -1,1 +0,0 @@
-__all__ = ["DBConfig.py"]
--- BDRC_DBLib/setup.py
+++
@@ -1,5 +0,0 @@
-from setuptools import setup
-
-setup(name='BDRC_DBLib', version='1.0a', packages=['DBLib', 'DBConfig', 'DBConfig.Tests'], url='',
-    license='Free to Everyone Forever', author='jimk', author_email='jimk@tbrc.org',
-    description='Common BDRC database library')
