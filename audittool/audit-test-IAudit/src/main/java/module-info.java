099ab886e04c962f3fbfa6341537f0cd7651fd81
Jim Katz <jimk@tbrc.org>
2022-01-09 22:25:57 -0500

    Modules first pass compiles

---
+++ audittool/audit-test-IAudit/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module audit.test.IAudit {
+    exports io.bdrc.am.audit.iaudit;
+    exports io.bdrc.am.audit.iaudit.message;
+
+    requires org.apache.commons.lang3;
+    requires slf4j.api;
+}
---
+++ audittool/audit-test-shell/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module audit.test.shell {
+    requires audit.test.IAudit;
+    requires slf4j.api;
+    requires commons.csv;
+    requires org.apache.logging.log4j;
+    requires org.apache.commons.lang3;
+    requires org.apache.logging.log4j.core;
+    requires commons.cli;
+    requires audit.test.lib;
+}
