a701928b0c6b113a8834c065aa9f3ad586ed7182
Jim Katz <33470789+jimk-bdrc@users.noreply.github.com>
2022-04-06 16:00:22 -0400

    Asset manager 152 (#157)
    * Update Iaudit pom.xml
    
    * One more try on Iaudit pom
    
    * One still more try
    
    * Rename possibly identical result file names
    
    asset-manager-152 Running Audit tool twice within a second crashes.
    
    * Rename possibly identical result file names
    
    asset-manager-152 Running Audit tool twice within a second crashes.

--- audittool/audit-test-shell/src/main/java/module-info.java
+++ audittool/audit-test-shell/src/main/java/module-info.java
@@ -2,7 +2,7 @@ module io.bdrc.audit.shell {
     exports io.bdrc.audit.shell;
     requires io.bdrc.audit.IAudit;
     requires io.bdrc.audit.tests;
-    requires org.apache.logging.log4j;
+    requires org.apache.commons.io;
     requires org.apache.commons.lang3;
     requires org.apache.logging.log4j.core;
     requires commons.cli;
@@ -11,5 +11,6 @@ module io.bdrc.audit.shell {
     // To avoid Cannot initialize scripting support because this JRE does not support it. java.lang.NoClassDefFoundError: javax/script/ScriptEngineManager
     // see https://stackoverflow.com/questions/53714010/log4j2-slf4j-and-java-11
     requires java.scripting;
+    requires org.apache.logging.log4j;
 
 }
